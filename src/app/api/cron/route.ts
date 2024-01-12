00b0b25903e3f8c91bdcc7399e15adc0804c620e
Juan Camilo Echeverri Salazar <juancami98.11@hotmail.com>
2024-01-12 13:39:23 -0500

    ffer

--- src/app/api/cron/route.ts
+++ src/app/api/cron/route.ts
@@ -11,7 +11,7 @@ import {
 import { NextResponse } from "next/server";
 
 // export const maxDuration = 100;
-// export const dynamic = "force-dynamic";
+export const dynamic = "force-dynamic";
 // export const revalidate = 0;
 
 export async function GET() {
